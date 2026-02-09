9827eac516b621c30c8762484566370b19e66fca
Manish Wagh <167597076+Mwagh5022@users.noreply.github.com>
2026-02-09 16:10:06 +0530

    Update linux-shell.sh

--- linux-shell.sh
+++ linux-shell.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 apt update
 apt install docker.io -y
+
+# for active HTTP need to install and start the service of apache2
+sudo apt install apache2 -y
+
+# for start service
+sudo systemctl start apache2
